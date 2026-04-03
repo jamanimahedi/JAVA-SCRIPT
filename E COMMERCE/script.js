40892eaccca1379b23b8a60c5b4f6e8f4d1dacad
JAMANI MAHEDIRAZA <jamanimahedi@gmail.com>
2026-04-03 14:28:26 +0530

    some changes are added

--- E COMMERCE/script.js
+++ E COMMERCE/script.js
@@ -1,195 +1,220 @@
 const products = [
   {
-    id: 1,
-    name: "Wireless Headphones",
-    price: 2999,
-    image: "https://images.pexels.com/photos/3394650/pexels-photo-3394650.jpeg",
-  },
-  {
-    id: 2,
-    name: "Smart Watch",
-    price: 4999,
-    image: "https://images.pexels.com/photos/437037/pexels-photo-437037.jpeg",
-  },
-  {
-    id: 3,
-    name: "Laptop",
-    price: 55999,
-    image: "https://images.pexels.com/photos/18105/pexels-photo.jpg",
-  },
-  {
-    id: 4,
-    name: "Gaming Mouse",
-    price: 1499,
-    image: "https://images.pexels.com/photos/2115257/pexels-photo-2115257.jpeg",
-  },
-  {
-    id: 5,
-    name: "Mechanical Keyboard",
+    id: 21,
+    name: "Smart Home Security Camera",
     price: 3499,
-    image: "https://images.pexels.com/photos/1772123/pexels-photo-1772123.jpeg",
+    image: "https://images.pexels.com/photos/3205735/pexels-photo-3205735.jpeg",
   },
   {
-    id: 6,
-    name: "Bluetooth Speaker",
-    price: 1999,
-    image: "https://images.pexels.com/photos/63703/pexels-photo-63703.jpeg",
-  },
-  {
-    id: 7,
-    name: "DSLR Camera",
-    price: 45999,
-    image: "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg",
-  },
-  {
-    id: 8,
-    name: "Running Shoes",
-    price: 2499,
-    image: "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg",
-  },
-  {
-    id: 9,
-    name: "Backpack",
+    id: 22,
+    name: "Electric Kettle Stainless Steel",
     price: 1299,
-    image: "https://images.pexels.com/photos/2905238/pexels-photo-2905238.jpeg",
+    image: "https://images.pexels.com/photos/1793037/pexels-photo-1793037.jpeg",
   },
   {
-    id: 10,
-    name: "Sunglasses",
-    price: 999,
-    image: "https://images.pexels.com/photos/46710/pexels-photo-46710.jpeg",
+    id: 23,
+    name: "Wireless Gaming Controller",
+    price: 2499,
+    image: "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg",
   },
   {
-    id: 11,
-    name: "Office Chair",
+    id: 24,
+    name: "Mini Projector HD",
+    price: 5999,
+    image: "https://images.pexels.com/photos/4145354/pexels-photo-4145354.jpeg",
+  },
+  {
+    id: 25,
+    name: "Air Fryer Digital",
     price: 6999,
-    image: "https://images.pexels.com/photos/1957478/pexels-photo-1957478.jpeg",
+    image: "https://images.pexels.com/photos/4108712/pexels-photo-4108712.jpeg",
   },
   {
-    id: 12,
-    name: "Coffee Mug",
-    price: 499,
-    image: "https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg",
+    id: 26,
+    name: "Robot Vacuum Cleaner",
+    price: 12999,
+    image: "https://images.pexels.com/photos/4108800/pexels-photo-4108800.jpeg",
   },
   {
-    id: 13,
-    name: "Mobile Phone",
-    price: 25999,
-    image: "https://images.pexels.com/photos/607812/pexels-photo-607812.jpeg",
+    id: 27,
+    name: "Smart Door Lock",
+    price: 8499,
+    image: "https://images.pexels.com/photos/279810/pexels-photo-279810.jpeg",
   },
   {
-    id: 14,
-    name: "Perfume",
+    id: 28,
+    name: "4K Action Camera",
+    price: 9999,
+    image: "https://images.pexels.com/photos/274973/pexels-photo-274973.jpeg",
+  },
+  {
+    id: 29,
+    name: "Electric Toothbrush",
+    price: 1999,
+    image: "https://images.pexels.com/photos/3735657/pexels-photo-3735657.jpeg",
+  },
+  {
+    id: 30,
+    name: "Portable Monitor 15.6 Inch",
+    price: 13999,
+    image: "https://images.pexels.com/photos/374074/pexels-photo-374074.jpeg",
+  },
+  {
+    id: 31,
+    name: "WiFi Range Extender",
     price: 1799,
-    image: "https://images.pexels.com/photos/965989/pexels-photo-965989.jpeg",
+    image: "https://images.pexels.com/photos/4219860/pexels-photo-4219860.jpeg",
   },
   {
-    id: 15,
-    name: "T-Shirt",
-    price: 799,
-    image: "https://images.pexels.com/photos/428338/pexels-photo-428338.jpeg",
-  },
+    id: 32,
+    name: "Smart Thermostat",
+    price: 5999,
+    image: "https://images.pexels.com/photos/259950/pexels-photo-259950.jpeg",
+  }
 ];
 
-products.forEach((p) => {
-  const productList = document.getElementById("product-list");
+const productContainer = document.getElementById("product-list");
 
-  productList.innerHTML += `
-    
-    <div class="col-md-4 mt-3">
-    <div class="card product-card shadow rounded-4">
-  <img src="${p.image}" class="card-img-top img-fluid rounded-4" alt="${p.name}">
-  <div class="card-body text-center">
+products.forEach((p) => {
+  productContainer.innerHTML += `
+
+  <div class="card text-center " style="height:30rem">
+  <img src="${p.image}" class="card-img-top" width="100%" height="300px"; alt="${p.name}">
+  <div class="card-body">
     <h5 class="card-title">${p.name}</h5>
-     <h4 class="card-text">₹${p.price} </h4>
-  <button class="btn btn-primary" onclick="addToCart(${p.id})" >Add to cart</button>
+    <p class="card-text">₹${p.price}</p>
+    <button class="btn btn-primary" onclick="addToCart(${p.id})">Add To Cart</button>
   </div>
 </div>
-    
-    </div>
 
-    `;
+  `;
 });
 
-// localStorage concept added
 
-// const data = { name: "electronic", qty: 1 };
-
-// localStorage.setItem("cartData", JSON.stringify(data));
-
-// const productData = localStorage.getItem("cartData");
-
-// console.log("productData", JSON.parse(productData));
-
-let cartItems = JSON.parse(localStorage.getItem("cartData")) || [];
-
-console.log("cartItems", cartItems);
 
 function addToCart(id) {
   try {
-    let product = cartItems.find((p) => p.id === id);
 
-    console.log("product already added", product);
+    let cartItems = JSON.parse(localStorage.getItem("cartdata")) || [];
+
+    let product = cartItems.find((prod) => prod.id === id);
 
     if (product) {
-      product.qty++;
+      product.quantity++;
     } else {
-      product = products.find((p) => p.id === id);
-
-      cartItems.push({ ...product, qty: 1 });
-
-      console.log("product new added", product);
+      product = products.find((prod) => prod.id === id);
+      cartItems.push({ ...product, quantity: 1 });
     }
 
-    localStorage.setItem("cartData", JSON.stringify(cartItems));
+    localStorage.setItem("cartdata", JSON.stringify(cartItems));
 
-    alert("item added in cart");
+
   } catch (error) {
     console.log(error);
   }
 }
 
-function showModal() {
-  try {
-    const cartList = document.getElementById("cartList");
+function ShowCart() {
+  let cart = JSON.parse(localStorage.getItem("cartdata")) || [];
 
-    let modal = new bootstrap.Modal(cartList);
+  let tbody = document.getElementById("t-body");
 
-    modal.show();
-    showCartList();
-  } catch (error) {
-    console.log(error);
-  }
-}
+  tbody.innerHTML = "";
 
-function showCartList() {
-  try {
-    const cartTable = document.getElementById("cartTable");
-
-    cartTable.innerHTML = "";
-
-    cartItems.forEach((p) => {
-      cartTable.innerHTML += `
-      
+  cart.forEach((item) => {
+    tbody.innerHTML += `
       <tr>
-      <td>${p.name}</td>
-      <td>
-      <div class="d-flex gap-2">
+        <td>${item.name}</td>
+        <td>
+      
+        <div class="d-flex ">
+          <button class="btn btn-success m-2" onclick="increase(${item.id})">+</button>
 
-      <button class="btn btn-outline-success" >+</button>
-      
-      <h5>${p.qty}</h5>
- <button class="btn btn-outline-danger" >-</button>
-      
-      </div>
-      </td>
-      <td>₹ ${p.price * p.qty}</td>
-      <td><button class="btn btn-outline-danger" >remove</button></td>
-      
+       <p class="fs-3 m-2"> ${item.quantity}</p>
+        
+        <button class="btn btn-danger m-2" onclick="decrease(${item.id})">-</button></div>
+
+        </td>
+
+
+        <td>₹${item.price * item.quantity}</td>
+
+
+        <td>
+        <button class="btn btn-outline-danger m-2" onclick="remove(${item.id})">Remove</button>
+        </td>
+
+
       </tr>
-      
+    `;
+  });
 
-      `;
-    });
-  } catch (error) {}
+  
+
+
+
+}
+
+function openCart(){
+  ShowCart();
+
+  const myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
+  myModal.show();
+}
+
+
+function increase(id) {
+
+  let data = JSON.parse(localStorage.getItem("cartdata"))
+
+  let item = data.find((p) => p.id === id)
+
+
+  if (item) {
+
+    item.quantity++;
+
+  }
+  localStorage.setItem("cartdata", JSON.stringify(data));
+
+
+  ShowCart()
+
+}
+
+function decrease(id) {
+
+  let data = JSON.parse(localStorage.getItem("cartdata")) || []
+
+  let item = data.find((p) => p.id === id)
+
+  if (item) {
+    item.quantity--;
+
+    if (item.quantity <= 0) {
+
+      data = data.filter((p) => p.id !== id)
+
+    }
+
+  }
+
+
+  localStorage.setItem("cartdata", JSON.stringify(data));
+
+
+  ShowCart()
+
+
+}
+
+function remove(id) {
+
+  let data = JSON.parse(localStorage.getItem("cartdata")) || []
+
+  data = data.filter((p) => p.id !== id)
+
+  localStorage.setItem("cartdata", JSON.stringify(data))
+
+  ShowCart()
 }
